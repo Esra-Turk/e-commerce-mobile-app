de1368d32b459d637a188ce6e80446a6b8431a1e
Esra-Turk <esra.turk@bil.omu.edu.tr>
2024-10-10 10:36:12 +0300

    feat: designed cart screen in storyboard and set up IBAction connections

--- e-commerce-app/ui/tableview/CartProductCell.swift
+++ e-commerce-app/ui/tableview/CartProductCell.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class CartProductCell: UITableViewCell {
 
+    @IBOutlet weak var customView: UIView!
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    @IBOutlet weak var productBrandLabel: UILabel!
+    @IBOutlet weak var productQuantityLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,5 +27,16 @@ class CartProductCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    
+    @IBAction func increaseQuantityButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func decreaseQuantityButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func removeProductButton(_ sender: Any) {
+    }
+    
+    
 }
--- e-commerce-app/ui/views/Cart.swift
+++ e-commerce-app/ui/views/Cart.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class Cart: UIViewController {
-
+    
+    @IBOutlet weak var numberOfProduct: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var discountLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +21,5 @@ class Cart: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
