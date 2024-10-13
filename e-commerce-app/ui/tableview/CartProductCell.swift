f534528615111d9d570d6ca5ce160bda156fd454
Esra-Turk <esra.turk@bil.omu.edu.tr>
2024-10-13 18:04:16 +0300

    feat: added context menu for delete confirmation on cart page

--- e-commerce-app/ui/tableview/CartProductCell.swift
+++ e-commerce-app/ui/tableview/CartProductCell.swift
@@ -15,11 +15,15 @@ class CartProductCell: UITableViewCell {
     @IBOutlet weak var productPriceLabel: UILabel!
     @IBOutlet weak var productBrandLabel: UILabel!
     @IBOutlet weak var productQuantityLabel: UILabel!
-    
+    @IBOutlet weak var removeProductButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        let interaction = UIContextMenuInteraction(delegate: self)
+        removeProductButton.isUserInteractionEnabled = true
+        removeProductButton.addInteraction(interaction)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -43,4 +47,28 @@ class CartProductCell: UITableViewCell {
     }
     
     
+}
+
+extension CartProductCell: UIContextMenuInteractionDelegate {
+    
+    // Context Menu
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+        
+        return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { suggestedActions in
+            
+            let deleteAction = UIAction(title: "Sil", image: UIImage(systemName: "trash"), attributes: .destructive) { action in
+                self.onRemoveButtonTapped?()  // Silme işlemi tetikleniyor
+            }
+            
+            let addToFavoritesAction = UIAction(title: "Sil ve Favorilere Ekle", image: UIImage(systemName: "heart")) { action in
+                print("Favorilere ekle butonuna basıldı")
+            }
+            
+            let cancelAction = UIAction(title: "İptal", image: UIImage(systemName: "xmark")) { action in
+                print("İptal butonuna basıldı")
+            }
+            
+            return UIMenu(title: "Ürünü silmek istediğine emin misin?", children: [deleteAction, addToFavoritesAction, cancelAction])
+        }
+    }
 }
